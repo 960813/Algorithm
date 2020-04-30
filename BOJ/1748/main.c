9463e642fef0d06ee2a9dac0abd249d232b96990
Keriel <keriel@jupiterflow.com>
2020-05-01 04:04:22 +0900

    BOJ #[1748] commit

---
+++ BOJ/1748/main.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int i, n, cnt = 0;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		int tmp = i;
+		while (tmp > 0) {
+			tmp /= 10;
+			cnt++;
+		}
+	}
+	printf("%d", cnt);
+
+
+	return 0;
+}
