3216bc1c55316d239f32bdd88de353a50ad9dea0
Keriel <keriel@jupiterflow.com>
2020-04-30 23:54:21 +0900

    BOJ #[8393] commit

--- BOJ/8393/main.c
+++ BOJ/8393/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-	int n = 0, a = 0, b = 0, i = 0;
+	int n, sum = 0;
 	scanf("%d", &n);
-	for (i = 0; i < n; i++) {
-		scanf("%d %d", &a, &b);
-		printf("%d", a + b);
+	for (int i = 1; i <= n; i++) {
+		sum += i;
 	}
+	printf("%d", sum);
 
 	return 0;
 }
