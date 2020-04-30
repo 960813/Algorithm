6c776abd276df61c9a8b08057d9fd1349ece963c
Keriel <keriel@jupiterflow.com>
2020-05-01 05:15:49 +0900

    BOJ #[2750] commit

---
+++ BOJ/2750/main.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+
+void sort(int* arr, int n) {
+	int i, j;
+	for (i = 0; i < n - 1; i++) {
+		int least = i;
+		for (j = i + 1; j < n; j++) {
+			if (arr[j] < arr[least])
+				least = j;
+		}
+		int tmp = arr[i];
+		arr[i] = arr[least];
+		arr[least] = tmp;
+	}
+}
+
+int arr[1000];
+int main(void) {
+	int n, i;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+
+	sort(arr, n);
+
+	for (i = 0; i < n; i++)
+		printf("%d\n", arr[i]);
+
+	return 0;
+}
