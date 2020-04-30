df60b13c15e01b3060cae8021124255d7cb455b5
Keriel <keriel@jupiterflow.com>
2020-05-01 05:08:34 +0900

    BOJ #[2748] commit

---
+++ BOJ/2748/main.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+long long arr[91];
+int main(void) {
+	int n, i;
+
+	scanf("%d", &n);
+
+	arr[1] = 1;
+	for (i = 2; i <= 90; i++)
+		arr[i] = arr[i - 1] + arr[i - 2];
+
+	printf("%lld", arr[n]);
+
+	return 0;
+}
