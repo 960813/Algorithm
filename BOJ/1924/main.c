97fe0668d819afad17ec68ab76ba2a6bd303431d
Keriel <keriel@jupiterflow.com>
2020-05-01 04:16:35 +0900

    BOJ #[1924] commit

---
+++ BOJ/1924/main.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int daysOfMonth[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+char* dayOfWeek[7] = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+
+int main(void) {
+	int month, day, i;
+	scanf("%d %d", &month, &day);
+
+	for (i = 1; i < month; i++) {
+		day += daysOfMonth[i];
+	}
+
+	printf("%s\n", dayOfWeek[day % 7]);
+
+	return 0;
+}
