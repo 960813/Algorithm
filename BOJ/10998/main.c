eda0ad802fa61692415a290bf9cda54bea9dfeb3
Keriel <keriel@jupiterflow.com>
2020-04-30 23:49:36 +0900

    BOJ #[10998] commit

--- BOJ/10998/main.c
+++ BOJ/10998/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-	int n1 = 0, n2 = 0;
-	scanf("%d %d", &n1, &n2);
-	printf("%d", n1 * n2);
+	short t = 0, a = 0, b = 0, i = 0;
+	scanf("%hd", &t);
+	for (i = 0; i < t; i++) {
+		scanf("%hd %hd", &a, &b);
+		printf("%hd\n", a + b);
+	}
+
 	return 0;
 }
