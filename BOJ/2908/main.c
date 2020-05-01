105fcd4221a106f6ee109a49c032889fc4f6e832
Keriel <keriel@jupiterflow.com>
2020-05-01 18:24:35 +0900

    BOJ #[2908] commit

---
+++ BOJ/2908/main.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int reverse(int n) {
+	int start = (n % 10) * 100;
+	int mid = (n % 100) / 10 * 10;
+	int end = n / 100;
+
+
+	return start + mid + end;
+}
+
+int main(void) {
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	a = reverse(a);
+	b = reverse(b);
+
+	printf("%d", a > b ? a : b);
+
+	return 0;
+}
