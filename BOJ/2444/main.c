30286152f71c3a71d11b370435781567cb8aa2d6
Keriel <keriel@jupiterflow.com>
2020-05-01 18:01:31 +0900

    BOJ #[2444] commit

---
+++ BOJ/2444/main.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int n, i, j;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++) {
+		for (j = i + 1; j < n; j++)
+			printf(" ");
+		for (j = 0; j <= i * 2; j++)
+			printf("*");
+		printf("\n");
+	}
+	for (i = n; i > 0; i--) {
+		for (j = i; j <= n; j++)
+			printf(" ");
+		for (j = 0; j <= (i - 2) * 2; j++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	return 0;
+}
