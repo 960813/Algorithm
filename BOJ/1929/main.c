d26cc31d2ca21c6fcca737323cba1cec33b7a880
Keriel <keriel@jupiterflow.com>
2020-05-01 15:32:55 +0900

    BOJ #[1929] commit

--- BOJ/1929/main.c
+++ BOJ/1929/main.c
@@ -1,15 +1,17 @@
 ﻿#include<stdio.h>
 
+int arr[1000001] = { 0,1 };
+
 int main(void) {
-	int i, j, m, n, a[1000001] = { 0,1 };
+	int i, j, m, n;
 	scanf("%d%d", &m, &n);
 
 	for (i = 2; i <= n; i++)
 		for (j = 2; i * j <= n; j++)
-			a[i * j] = 1;
+			arr[i * j] = 1;
 
 	for (i = m; i <= n; i++)
-		if (!a[i]) printf("%d\n", i);
+		if (!arr[i]) printf("%d\n", i);
 
 	return 0;
 }
