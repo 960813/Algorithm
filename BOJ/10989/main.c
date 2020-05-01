5a00f88939081d003217ae62a4ad0b4ab5557f87
Keriel <keriel@jupiterflow.com>
2020-05-01 16:28:37 +0900

    BOJ #[10989] commit

---
+++ BOJ/10989/main.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int arr[10001];
+int main(void) {
+	int n, i, j, max = 0, temp;
+
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++) {
+		scanf("%d", &temp);
+		arr[temp]++;
+		if (temp > max) max = temp;
+	}
+
+
+	for (i = 1; i <= max; i++)
+		for (j = 1; j <= arr[i]; j++)
+			printf("%d\n", i);
+
+	return 0;
+}
