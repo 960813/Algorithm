32886488209f2258b1fab3566e468eb7d976a0bd
Keriel <keriel@jupiterflow.com>
2020-05-01 15:40:17 +0900

    BOJ #[1978] commit

---
+++ BOJ/1978/main.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int isPrime(int n) {
+	if (n <= 1) return 0;
+	int i;
+	for (i = 2; i <= sqrt(n); i++)
+		if (n % i == 0)
+			return 0;
+	return 1;
+}
+
+int main(void) {
+	int n, input, cnt = 0, i;
+
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &input);
+		if (isPrime(input))
+			cnt++;
+	}
+	printf("%d", cnt);
+
+	return 0;
+}
