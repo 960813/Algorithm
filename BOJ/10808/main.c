64943c76e05b92861bd427a41fbd0cd343ee7bb3
Keriel <keriel@jupiterflow.com>
2020-05-01 13:01:15 +0900

    BOJ #[10808] commit

---
+++ BOJ/10808/main.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+char arr[100];
+int ans[26];
+int main(void) {
+	int i;
+	scanf("%s", arr);
+
+	for (i = 0; i < (int)strlen(arr); i++)
+		ans[arr[i] - 97]++;
+
+	for (i = 0; i < 26; i++)
+		printf("%d ", ans[i]);
+
+
+	return 0;
+}
