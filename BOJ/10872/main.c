1ddd93136e87239248a86c484ba0dc5757e031be
Keriel <keriel@jupiterflow.com>
2020-05-01 12:58:31 +0900

    BOJ #[10872] commit

---
+++ BOJ/10872/main.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int f(int n) {
+	int i, ret = 1;
+
+	if (n == 0) return 1;
+
+	for (i = 1; i <= n; i++)
+		ret *= i;
+
+	return ret;
+}
+
+int main(void) {
+	int N;
+	scanf("%d", &N);
+
+	printf("%d\n", f(N));
+
+	return 0;
+}
