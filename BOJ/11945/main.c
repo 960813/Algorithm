35262c74199621a0f0031852cf52c33c2444df0e
Keriel <keriel@jupiterflow.com>
2020-05-01 12:49:16 +0900

    BOJ #[11945] commit

---
+++ BOJ/11945/main.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+char arr[10][10];
+int main(void) {
+	int N, M, i, j;
+
+	scanf("%d %d", &N, &M);
+
+	for (i = 0; i < N; i++) {
+		scanf("%s", &arr[i]);
+
+		for (j = 0; j < M; j++)
+			printf("%c", arr[i][M - 1 - j]);
+
+		printf("\n");
+	}
+
+	return 0;
+}
