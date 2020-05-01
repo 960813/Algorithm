9942b57f823c9c67c767806c4db2b9718dd69487
Keriel <keriel@jupiterflow.com>
2020-05-01 19:09:03 +0900

    BOJ #[5338] commit

---
+++ BOJ/5338/main.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	printf("       _.-;;-._\n");
+	printf("'-..-'|   ||   |\n");
+	printf("'-..-'|_.-;;-._|\n");
+	printf("'-..-'|   ||   |\n");
+	printf("'-..-'|_.-''-._|\n");
+
+	return 0;
+}
