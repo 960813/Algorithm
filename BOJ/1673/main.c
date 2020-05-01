1e3d0efc2f89757586da47bd22e46609892e5766
Keriel <keriel@jupiterflow.com>
2020-05-01 15:57:56 +0900

    BOJ #[1673] commit

---
+++ BOJ/1673/main.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int n, k;
+
+	while (scanf("%d %d", &n, &k) != EOF) {
+		int ans = n;
+		while (n > 0) {
+			n += -k + 1;
+			ans++;
+		}
+		printf("%d\n", --ans);
+	}
+
+	return 0;
+}
