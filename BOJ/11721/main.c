6007efa3aaefd86b155e4251ce379fe0372ab2b1
Keriel <keriel@jupiterflow.com>
2020-05-01 12:51:47 +0900

    BOJ #[11721] commit

---
+++ BOJ/11721/main.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+char arr[100];
+int main(void) {
+	int i;
+	scanf("%s", arr);
+
+	for (i = 1; i <= (int)strlen(arr); i++) {
+		printf("%c", arr[i - 1]);
+		if (i % 10 == 0)
+			printf("\n");
+	}
+
+	return 0;
+}
