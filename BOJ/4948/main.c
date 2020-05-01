c0a46883af393f80f9dc79778a90218e21a5c7fc
Keriel <keriel@jupiterflow.com>
2020-05-01 15:47:55 +0900

    BOJ #[4948] commit

--- BOJ/4948/main.c
+++ BOJ/4948/main.c
@@ -7,6 +7,7 @@ int isPrime(int n) {
 	for (i = 2; i <= sqrt(n); i++)
 		if (n % i == 0)
 			return 0;
+
 	return 1;
 }
 
@@ -18,10 +19,10 @@ int main(void) {
 		if (!n) break;
 
 		cnt = 0;
-		for (i = n + 1; i <= 2 * n; i++) {
+		for (i = n + 1; i <= 2 * n; i++) 
 			if (isPrime(i))
 				cnt++;
-		}
+		
 		printf("%d\n", cnt);
 	}
 
