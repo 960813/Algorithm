fc25de31ed255e6d8afa7a38a60f97689dd2ecf1
Keriel <keriel@jupiterflow.com>
2020-05-01 16:05:42 +0900

    BOJ #[2747] commit

---
+++ BOJ/2747/main.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int arr[46];
+
+int f(int n) {
+	if (n <= 1) return n;
+
+	if (arr[n]) return arr[n];
+
+	return arr[n - 1] + arr[n - 2];
+}
+
+int main(void) {
+	int n, i;
+
+	scanf("%d", &n);
+
+	for (i = 0; i <= 45; i++) 
+		arr[i] = f(i);
+	
+	printf("%d", f(n));
+
+	return 0;
+}
