4ff43a8f4b9b998b92cb84464f3e3a660e26c4f4
Keriel <keriel@jupiterflow.com>
2020-05-01 15:08:59 +0900

    BOJ #[2490] commit

---
+++ BOJ/2490/main.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int arr[4];
+char* ans[5] = { "D", "C", "B", "A", "E" };
+int main(void) {
+	int i, tc = 3;
+	while (tc--) {
+		int sum = 0;
+		for (i = 0; i < 4; i++) {
+			scanf("%d", &arr[i]);
+			sum += arr[i];
+		}
+		printf("%s\n", ans[sum]);
+	}
+	return 0;
+}
