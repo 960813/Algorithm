b63df33d316b805c19f961c48c4b764c36e0113b
Keriel <keriel@jupiterflow.com>
2020-05-01 15:00:49 +0900

    BOJ #[2231] commit

---
+++ BOJ/2231/main.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int n, ans, i;
+
+	scanf("%d", &n);
+	ans = n;
+
+	for (i = 1; i < n; i++) {
+		int t = i, r = i;
+		while (t > 0) {
+			r += t % 10;
+			t /= 10;
+		}
+		if (r == n && i < ans)
+			ans = i;
+	}
+	printf("%d", ans == n ? 0 : ans);
+
+	return 0;
+}
