34f57046982d55daadac9d6c5f30dfd7a58317a1
Keriel <keriel@jupiterflow.com>
2020-05-01 19:27:07 +0900

    BOJ #[10991] commit

---
+++ BOJ/10991/main.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int n, i, j, flag;
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n - i - 1; j++)
+			printf(" ");
+		flag = 1;
+		for (j = 0; j <= i * 2; j++)
+			printf("%c", (flag *= -1) == -1 ? '*' : ' ');
+		printf("\n");
+	}
+	return 0;
+}
