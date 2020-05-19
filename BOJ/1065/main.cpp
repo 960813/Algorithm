ee85241d96e06450868c6c83fa1b8feaef590de9
Keriel <keriel@jupiterflow.com>
2020-05-19 20:59:23 +0900

    BOJ #[1065] commit

--- BOJ/1065/main.cpp
+++ BOJ/1065/main.cpp
@@ -3,6 +3,15 @@
 #include <algorithm>
 using namespace std;
 
+bool hansu(int x) {
+	int num100 = x / 100;
+	int num10 = (x % 100) / 10;
+	int num1 = (x % 100) % 10;
+	if ((num100 - num10) == (num10 - num1))
+		return true;
+	return false;
+}
+
 int n, x, tmp, cnt = 99;
 int main() {
 	scanf("%d", &n);
@@ -12,10 +21,7 @@ int main() {
 		return 0;
 	}
 	for (x = 100; x <= n; x++) {
-		int num100 = x / 100;
-		int num10 = (x % 100) / 10;
-		int num1 = (x % 100) % 10;
-		if ((num100 - num10) == (num10 - num1))
+		if(hansu(x))
 			cnt++;
 	}
 	printf("%d", cnt);
