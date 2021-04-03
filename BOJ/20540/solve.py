2e7cca3f490aa7d3511c9103daa9679137534a20
Taeyang Jin <sun@jupiterflow.com>
2021-04-03 23:52:27 +0900

    BOJ #[20540] commit

---
+++ BOJ/20540/solve.py
@@ -0,0 +1,5 @@
+mbti = input()
+print('I' if mbti[0] == 'E' else 'E', end='')
+print('S' if mbti[1] == 'N' else 'N', end='')
+print('T' if mbti[2] == 'F' else 'F', end='')
+print('J' if mbti[3] == 'P' else 'P', end='')
