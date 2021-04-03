1193e5806b5359ecbc9e5f5ff7a755d83a4effe7
Taeyang Jin <sun@jupiterflow.com>
2021-04-03 22:40:40 +0900

    BOJ #[2480] commit

---
+++ BOJ/2480/solve.py
@@ -0,0 +1,10 @@
+lst = sorted(list(map(int, input().split())))
+
+if lst[0] == lst[1] == lst[2]:
+    print(10000 + lst[0] * 1000)
+elif lst[0] == lst[1]:
+    print(1000 + lst[0] * 100)
+elif lst[1] == lst[2]:
+    print(1000 + lst[1] * 100)
+else:
+    print(max(lst) * 100)
